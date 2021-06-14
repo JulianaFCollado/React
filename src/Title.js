a166e3a2108a2ad420b0f830ed4665fbbf05484a
Juli F Collado <julifcollado@gmail.com>
2021-06-14 19:09:17 -0300

    Creacion de una app React

---
+++ src/Title.js
@@ -0,0 +1,20 @@
+//es un componente
+
+function Title (props){
+    console.log(props.text)
+    let text = "Una variable con texto"
+    return (
+       //cualquier bloque html
+       <div>
+           <h1>Hola react!</h1> 
+           <h1>{props.text}</h1>
+           <p>Un parrafo estatico </p>
+           <p>{text}</p>
+           !---//esto es dinamico
+
+       </div>
+    )
+}
+export default Title
+
+//lo uso en app.js
